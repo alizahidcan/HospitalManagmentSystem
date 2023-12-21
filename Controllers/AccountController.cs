2ba156c2106ac4708f20d49da738da9d329bee45
alizahidcan <104202546+alizahidcan@users.noreply.github.com>
2023-12-21 22:40:41 +0300

    added Login and Register pages
    login and register pages are completed,users model is changed,db and login-register pages will be connected to each other

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using HospitalManagment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalManagment.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(users model)
+        {
+            if (ModelState.IsValid) { }
+
+            return View(model);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(users model)
+        {
+            if (ModelState.IsValid) { }
+            return View(model);
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
--- Models/users.cs
+++ Models/users.cs
@@ -5,10 +5,23 @@ namespace HospitalManagment.Models
     public class users
     {
         [Key] public int Id { get; set; }
-
+        [Required]
         public string Name { get; set; }
 
         public int age { get; set; }
 
+        public string problem { get; set; }
+        [MinLength(10)]
+        [MaxLength(10)]
+        public int phone { get; set; }
+        
+        [Required]
+        [MinLength(4)]
+        public string password {  get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [Compare(nameof(password))]
+        public string rePassword { get; set; }
     }
 }
